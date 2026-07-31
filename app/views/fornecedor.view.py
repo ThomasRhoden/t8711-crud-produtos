5025ed94d8d8a98f91d1b5db19a60304eb7338d5
ThomasRhoden <rhodenthomas@gmail.com>
2026-07-31 20:27:35 -0300

    Commit aula 31/07 - Desenvolvimento da interface de sistema

--- app/views/fornecedor.view.py
+++ app/views/fornecedor.view.py
@@ -7,6 +7,7 @@ from app.models.fornecedor import Fornecedor
 import tkinter as tk
 from tkinter import messagebox
 
+
 class Fornecedor_View:
     def __init__(self, root):
         self.root = root
@@ -19,13 +20,13 @@ class Fornecedor_View:
         self.root.title("CRUD de Fornecedores")
         self.root.geometry("800x600")
         self.root.resizable(False, False)
-        self.root.configure(bg="#247a15")
-        
+  
+
     def criar_componentes(self):
         self.lbl_titulo = tk.Label(
             self.root,
             text = "Cadastro de Fornecedores",
-            font = ("Arial", 16, "bold")
+            font = ("Arial", 16, "bold"),
         )
         self.lbl_titulo.grid(
             row = 0,
@@ -41,9 +42,178 @@ class Fornecedor_View:
         self.frm_dados.grid(
             row = 1,
             column = 0,
+            columnspan=4,
+            padx = 10,
+            pady = 5,
+            sticky = "w"
+        )
+        self.lbl_id = tk.Label(
+            self.frm_dados,
+            text = "ID:"
+        )
+        self.lbl_id.grid(
+            row = 0,
+            column = 0,
+            padx = 5,
+            pady = 5,
+            sticky = "w"
+        )
+        self.txt_id = tk.Entry(
+            self.frm_dados,
+            width = 10,
+            state = "readonly"
+        )
+        self.txt_id.grid(
+            row = 0,
+            column= 1,
+            padx = 5,
+            pady = 5,
+            sticky = "w"
+        )
+        self.lbl_razao_social = tk.Label(
+            self.frm_dados,
+            text = "Razão social:"
+        )
+        self.lbl_razao_social.grid(
+            row = 1,
+            column = 0,
+            padx = 5,
+            pady = 5,
+            sticky = "w"
+        )
+        self.txt_razao_social = tk.Entry(
+            self.frm_dados,
+            width = 40
+        )
+        self.txt_razao_social.grid(
+            row = 1,
+            column = 1,
+            padx = 5,
+            pady = 5,
+            sticky = "w"
+        )
+        self.lbl_nome_fantasia = tk.Label(
+            self.frm_dados,
+            text = "Nome fantasia:"
+        )
+        self.lbl_nome_fantasia.grid(
+            row = 1,
+            column = 2,
+            padx = 5,
+            pady = 5,
+            sticky = "w"
+        )
+        self.txt_nome_fantasia = tk.Entry(
+            self.frm_dados,
+            width = 40
+        )
+        self.txt_nome_fantasia.grid(
+            row = 1,
+            column = 3,
+            padx = 5,
+            pady = 5,
+            sticky = "w"
+        )
+        self.lbl_cnpj = tk.Label(
+            self.frm_dados,
+            text = "CNPJ:"
+        )
+        self.lbl_cnpj.grid(
+            row = 2,
+            column = 0,
+            padx = 5,
+            pady = 5,
+            sticky = "w"
+        )
+        self.txt_cnpj = tk.Entry(
+            self.frm_dados,
+            width = 20
+        )
+        self.txt_cnpj.grid(
+            row = 2,
+            column = 1,
+            padx = 5,
+            pady = 5,
+            sticky = "w"
+        )
+        self.lbl_sla = tk.Label(
+            self.frm_dados,
+            text = "SLA de atendimento:"
+        )
+        self.lbl_sla.grid(
+            row = 2,
+            column = 2,
+            padx = 5,
+            pady = 5,
+            sticky = "w"
+        )
+        self.txt_sla = tk.Entry(
+            self.frm_dados,
+            width = 20
+        )
+        self.txt_sla.grid(
+            row = 2,
+            column = 3,
+            padx = 5,
+            pady = 5,
+            sticky = "w"
+        )
+        self.frm_botoes = tk.Frame(
+            self.root,
+            border = 2,
+            relief = "groove"
+        )
+        self.frm_botoes.grid(
+            row = 2,
+            column = 0,
+            padx = 10,
+            pady = 5,
             columnspan = 4
         )
-
+        self.btn_novo = tk.Button(
+            self.frm_botoes,
+            text = "Novo",
+            width = 15
+        )
+        self.btn_novo.grid(
+            row = 0,
+            column = 0,
+            padx = 5,
+            pady = 5
+        )
+        self.btn_alterar = tk.Button(
+            self.frm_botoes,
+            text = "Alterar",
+            width = 15
+        )
+        self.btn_alterar.grid(
+            row = 0,
+            column = 1,
+            padx = 5,
+            pady = 5
+        )        
+        self.btn_excluir = tk.Button(
+            self.frm_botoes,
+            text = "Excluir",
+            width = 15
+        )
+        self.btn_excluir.grid(
+            row = 0,
+            column = 2,
+            padx = 5,
+            pady = 5
+        )   
+        self.btn_fechar = tk.Button(
+            self.frm_botoes,
+            text = "Fechar",
+            width = 15
+        )
+        self.btn_fechar.grid(
+            row = 0,
+            column = 3,
+            padx = 5,
+            pady = 5
+        )                 
     def configurar_treeview(self):
         pass
     def configurar_eventos(self):
